672cef5cbffefb837fd6b92bbb380399cdb05664
Andre Hahn <andre.hahn@tfgco.com>
2016-06-16 19:50:17 -0300

    register_user plugin now parsing message and adding user

--- plugins/register_user.lua
+++ plugins/register_user.lua
@@ -1,9 +1,18 @@
 local redis = require 'redis_module'
 local password = require 'password'
+local json = require 'json'
 
 function run_plugin(topic, payload)
-  err, ret = password.generate_hash("password")
-  err, ret = redis.execute("set", 2, "teste", ret)
+  local json_message = json.decode(payload)
+  --message: {"payload": {"message": "register", "username": "username",
+  --          "password": "userpass"}}
+  local username = json_message["payload"]["username"]
+  local pass = json_message["payload"]["password"]
+  err, ret = password.generate_hash(pass)
+  if err ~= nil then
+    return err, 1
+  end
+  err, ret = redis.execute("set", 2, username, ret)
   if err ~= nil then
     return err, 1
   end
