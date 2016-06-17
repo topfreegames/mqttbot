ea7c0d783ec7ec49354f806fc98b69c7636154f7
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2016-06-17 14:29:06 -0300

    add acl plugin

--- plugins/add_user_subscription_acl.lua
+++ plugins/add_user_subscription_acl.lua
@@ -1,5 +1,4 @@
 local redis = require 'redis_module'
-local password = require 'password'
 local json = require 'json'
 
 function register_subscriptions(username, subscriptions)
@@ -14,18 +13,9 @@ end
 
 function run_plugin(topic, payload)
   local json_message = json.decode(payload)
-  --message: {"payload": {"message": "register", "username": "username", "password": "userpass", subscriptions: ["subs1", "subs2"]}}
+  --message: {"payload": {"message": "aclpermitsubscription", "username": "username", subscriptions: ["subs1", "subs2"]}}
   local username = json_message["payload"]["username"]
-  local pass = json_message["payload"]["password"]
   local subscriptions = json_message["payload"]["subscriptions"]
-  err, ret = password.generate_hash(pass)
-  if err ~= nil then
-    return err, 1
-  end
-  err, ret = redis.execute("set", 2, username, ret)
-  if err ~= nil then
-    return err, 1
-  end
   if subscriptions ~= nil then
     err, ret = register_subscriptions(username, subscriptions)
     if err ~= nil then
