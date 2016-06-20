d90554e8ac29a94cb24235ad606189406c3a484e
Andre Hahn <andre.hahn@tfgco.com>
2016-06-20 14:12:11 -0300

    Added dummy tests to all folders

--- bot/utils_test.go
+++ bot/utils_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func RouteTest(t *testing.T) {
+func TestRoute(t *testing.T) {
 	route := []string{"a", "b"}
 	topic := []string{"a", "b"}
 	if !RouteIncludesTopic(route, topic) {
@@ -21,9 +21,15 @@ func RouteTest(t *testing.T) {
 	if RouteIncludesTopic(route, topic) {
 		t.Fail()
 	}
+
+	route = []string{"a", "b", "c"}
+	topic = []string{"a", "b", "d"}
+	if RouteIncludesTopic(route, topic) {
+		t.Fail()
+	}
 }
 
-func WildcardTest(t *testing.T) {
+func TestWildcard(t *testing.T) {
 	route := []string{"#"}
 	topic := []string{"anything", "really"}
 	if !RouteIncludesTopic(route, topic) {
