a824e831418b04a0330b2f2bb7c653901b805ed3
Andre Hahn <andre.hahn@tfgco.com>
2016-06-21 16:37:39 -0300

    Fixed some of the code for the esclient and added a simple test

--- es/es_client_test.go
+++ es/es_client_test.go
@@ -2,7 +2,17 @@ package es
 
 import (
 	"testing"
+
+	"github.com/spf13/viper"
 )
 
 func TestES(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("../config/test.yml")
+	config.AutomaticEnv()
+	config.ReadInConfig()
+	client := GetESClient(config)
+	if client == nil {
+		t.Fail()
+	}
 }
--- modules/plugins_persistence_module.go
+++ modules/plugins_persistence_module.go
@@ -12,6 +12,7 @@ import (
 	"gopkg.in/topfreegames/elastic.v2"
 )
 
+// PayloadStruct contains the fields of a payload
 type PayloadStruct struct {
 	From      string `json:"from"`
 	Message   string `json:"message"`
@@ -24,7 +25,7 @@ type Message struct {
 	Topic   string        `json:"topic"`
 }
 
-var ESClient *elastic.Client
+var esclient *elastic.Client
 
 func PersistenceModuleLoader(L *lua.LState) int {
 	configurePersistenceModule()
@@ -39,7 +40,7 @@ var persistenceModuleExports = map[string]lua.LGFunction{
 }
 
 func configurePersistenceModule() {
-	ESClient = es.GetESClient()
+	esclient = es.GetESClient(Config)
 }
 
 func IndexMessage(L *lua.LState) int {
@@ -49,12 +50,12 @@ func IndexMessage(L *lua.LState) int {
 	var message Message
 	json.Unmarshal([]byte(payload.String()), &message)
 	message.Topic = topic.String()
-	if _, err := ESClient.Index().Index("chat").Type("message").BodyJson(message).Do(); err != nil {
+	if _, err := esclient.Index().Index("chat").Type("message").BodyJson(message).Do(); err != nil {
 		L.Push(lua.LString(fmt.Sprintf("%s", err)))
 		L.Push(L.ToNumber(1))
 		return 2
 	}
-	logger.Logger.Debug(fmt.Sprintf("Message persisted: %s", message))
+	logger.Logger.Debug(fmt.Sprintf("Message persisted: %v", message))
 	L.Push(lua.LNil)
 	L.Push(L.ToNumber(0))
 	return 2
@@ -65,9 +66,12 @@ func QueryMessages(L *lua.LState) int {
 	limit := L.Get(-2)
 	start := L.Get(-1)
 	L.Pop(3)
-	logger.Logger.Debug(fmt.Sprintf("Getting %d messages from topic %s, starting with %d", int(lua.LVAsNumber(limit)), topic.String(), int(lua.LVAsNumber(start))))
+	logger.Logger.Debug(fmt.Sprintf("Getting %d messages from topic %s, starting with %d",
+		int(lua.LVAsNumber(limit)), topic.String(), int(lua.LVAsNumber(start))))
 	termQuery := elastic.NewQueryStringQuery(fmt.Sprintf("topic:\"%s\"", topic.String()))
-	searchResults, err := ESClient.Search().Index("chat").Query(termQuery).Sort("payload.timestamp", false).From(int(lua.LVAsNumber(start))).Size(int(lua.LVAsNumber(limit))).Do()
+	searchResults, err := esclient.Search().Index("chat").Query(termQuery).
+		Sort("payload.timestamp", false).From(int(lua.LVAsNumber(start))).
+		Size(int(lua.LVAsNumber(limit))).Do()
 	if err != nil {
 		L.Push(lua.LString(fmt.Sprintf("%s", err)))
 		L.Push(L.ToNumber(1))
