f1a3efdc2417ddb239aba5b885e1f8c47a431932
Mauricio Banduk <banduk@gmail.com>
2016-10-17 21:23:20 -0200

    Fix dockerfile

--- start_docker.sh
+++ start_docker.sh
@@ -4,9 +4,9 @@ if [ "$MQTTBOT_API_TLS" == "true" ]
 then
   echo -e $MQTTBOT_API_CERT > $MQTTBOT_API_CERTFILE
   echo -e $MQTTBOT_API_KEY > $MQTTBOT_API_KEYFILE
-  PORT=4443
+  export PORT=4443
 else
-  PORT=5000
+  export PORT=5000
 fi
 
 /go/bin/mqttbot start --bind 0.0.0.0 --port ${PORT} --config ${MQTTBOT_CONFIG_FILE}
